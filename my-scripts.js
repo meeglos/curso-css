352e55602919456151a250bfc248f8f084e9c37a
Miguel Rodriguez <miguel@rodriguez.me>
2016-09-30 02:53:54 +0200

    Hovered colored

--- my-scripts.js
+++ my-scripts.js
@@ -1,16 +1,17 @@
-var links = document.getElementsByTagName('a');
+// var links = document.getElementsByTagName('a');
 
 /*document.addEventListener('DOMContentLoaded',function() {
 		for (var i = 0; i < links.length; i++) {
 			window.alert(links[i]);
 		}
 })*/
-
-$('.collapse').on('shown.bs.collapse', function (e) {
-  $('.collapse').not(this).removeClass('in');
-});
-
-$('[data-toggle=collapse]').click(function (e) {
-  $('[data-toggle=collapse]').parent('li').removeClass('active');
-  $(this).parent('li').toggleClass('active');
-});
+/*$('a').hover(
+    function() {
+    	$(this).css('background-color', '#513235');
+    }, function () {
+    	$(this).css('background-color', '#333333');
+    }
+    // $(this).parent().parent().find('a').css('background-color', '#5512F3');
+    // $(this).siblings().css('background-color', '#AB99D5');
+);
+*/
